384f9e6ecca31b2554d0bf16822191348c8ebe8e
Roman Stoffel <roman.stoffel@gamlor.info>
2011-10-17 18:49:43 +0200

    rs: sending works

--- akka-mobile-test/src/test/scala/info/gamlor/remoting/EchoActorSpec.scala
+++ akka-mobile-test/src/test/scala/info/gamlor/remoting/EchoActorSpec.scala
@@ -1,28 +1,48 @@
 package info.gamlor.remoting
 
-import org.scalatest.matchers.MustMatchers
 import org.scalatest.{WordSpec, Spec}
 import akka.testkit.TestKit
 import akka.actor.Actor
 import TestServer._
+import java.util.concurrent.{TimeUnit, CountDownLatch}
+import org.scalatest.matchers.{ShouldMatchers, MustMatchers}
+import org.scalatest.matchers.ShouldMatchers._
 
 /**
  * @author roman.stoffel@gamlor.info
  * @since 13.10.11
  */
 
-class EchoActorSpec extends WordSpec with MustMatchers with TestKit {
+class EchoActorSpec extends WordSpec with ShouldMatchers with TestKit {
 
 
   "The Actor must " must {
 
-    "send back a Hello " in {
+    "receives message " in {
 
       withRunningServer(ctx => {
-        ctx.register("echo", Actor.actorOf[EchoActor]);
+        val barrier = new CountDownLatch(1);
+        val local = Actor.actorOf(new EchoActor(Some(barrier))).start();
+        ctx.register("echo", local);
         val echo = Actor.remote.actorFor("echo", "localhost", ctx.port);
-        echo ! "Hello"
-        expectMsg("Hello")
+        echo ! "Hello-Receive-Only"
+
+
+        val receivedMsg = barrier.await(6,TimeUnit.SECONDS)
+        receivedMsg should be( true )
+      })
+    }
+
+    "be able to reply " in {
+
+      withRunningServer(ctx => {
+        val local = Actor.actorOf(new EchoActor(None)).start();
+        ctx.register("echo", local);
+        val echo = Actor.remote.actorFor("echo", "localhost", ctx.port);
+        echo ! "Ask"
+
+
+        expectMsg("Answer for Ask")
       })
     }
 
@@ -30,11 +50,14 @@ class EchoActorSpec extends WordSpec with MustMatchers with TestKit {
 
 }
 
-class EchoActor extends Actor {
+class EchoActor(barrier : Option[CountDownLatch] = null) extends Actor {
   protected def receive = {
 
+    case "Hello-Receive-Only" => {
+      barrier.get.countDown()
+    }
     case x: String => {
-      self.reply(x)
+      self.reply("Answer for "+x)
     }
 
   }
