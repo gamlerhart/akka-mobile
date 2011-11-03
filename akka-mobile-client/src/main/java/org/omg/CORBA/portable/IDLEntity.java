05911c60fa0f38f3f001e4f04fd7d1c820b23ac8
Roman Stoffel <roman.stoffel@gamlor.info>
2011-11-03 17:17:17 +0100

    moving stuff around

---
+++ akka-mobile-client/src/main/java/org/omg/CORBA/portable/IDLEntity.java
@@ -0,0 +1,9 @@
+package org.omg.CORBA.portable;
+
+/**
+ * Satisfy class loader and compiler on Android. Isn't used in practise.
+ * @author roman.stoffel@gamlor.info
+ * @since 03.11.11
+ */
+public interface IDLEntity {
+}
